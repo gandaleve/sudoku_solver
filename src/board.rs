637e281db7e2f6927d6f974292d2cc44731fab0f
Inderpal Singh <gandaleve@gmail.com>
2023-02-24 12:12:05 -0800

    impl BoardBuilder

--- src/board.rs
+++ src/board.rs
@@ -6,3 +6,20 @@ pub struct Board {
 }
 
 impl Board {}
+
+pub struct BoardBuilder {
+	matrix: RefCell<[[i8; 9]; 9]>,
+}
+impl BoardBuilder {
+	pub fn file(&self, fd: File) {
+		todo!()
+	}
+	pub fn array(&self, val: [[i8; 9]; 9]) {
+		self.matrix.swap(&RefCell::new(val));
+	}
+	pub fn build(self) -> Board {
+		Board {
+			matrix: self.matrix,
+		}
+	}
+}
