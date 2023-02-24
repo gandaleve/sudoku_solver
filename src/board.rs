5618e36fda715cb02f7f20ecebb3a373f9aab4d0
Inderpal Singh <gandaleve@gmail.com>
2023-02-24 12:37:22 -0800

    fn added to check if position is valid and non-conflicting

--- src/board.rs
+++ src/board.rs
@@ -5,7 +5,29 @@ pub struct Board {
 	matrix: RefCell<[[u8; 9]; 9]>,
 }
 
-impl Board {}
+impl Board {
+	fn position_valid(&self, row: usize, col: usize, val: u8) -> bool {
+		let m = self.matrix.borrow();
+
+		for cell in 0..9 {
+			if m[row][cell] == val || m[cell][col] == val {
+				return false;
+			}
+		}
+
+		let u = (row / 3) * 3;
+		let v = (col / 3) * 3;
+
+		for x in &m[u..u + 3] {
+			for y in v..v + 3 {
+				if x[y] == val {
+					return false;
+				}
+			}
+		}
+		true
+	}
+}
 
 pub struct BoardBuilder {
 	matrix: RefCell<[[u8; 9]; 9]>,
