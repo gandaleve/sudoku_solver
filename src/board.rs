99d44ade5fa55476c1d94f1bfdbb8f2a297d230d
Inderpal Singh <gandaleve@gmail.com>
2023-02-24 12:20:58 -0800

    changed cell value from i8 to u8

--- src/board.rs
+++ src/board.rs
@@ -2,19 +2,19 @@ use std::cell::RefCell;
 use std::fs::File;
 
 pub struct Board {
-	matrix: RefCell<[[i8; 9]; 9]>,
+	matrix: RefCell<[[u8; 9]; 9]>,
 }
 
 impl Board {}
 
 pub struct BoardBuilder {
-	matrix: RefCell<[[i8; 9]; 9]>,
+	matrix: RefCell<[[u8; 9]; 9]>,
 }
 impl BoardBuilder {
 	pub fn file(&self, fd: File) {
 		todo!()
 	}
-	pub fn array(&self, val: [[i8; 9]; 9]) {
+	pub fn array(&self, val: [[u8; 9]; 9]) {
 		self.matrix.swap(&RefCell::new(val));
 	}
 	pub fn build(self) -> Board {
